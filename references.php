99098eccceb74c9e310e8c7d194ea8a30682880d
lauriane chauvet <pixelleart@gmail.com>
2020-03-26 11:10:55 +0100

    add title on caroussel on page reference

--- references.php
+++ references.php
@@ -1,6 +1,37 @@
 <?php include ('header.inc.php')?>
-<?php include ('caroussel.inc.php')?>
 
+    <!-- CAROUSSEL -->
+
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+
+            <div class="container-fluid px-0 title-caroussel">
+                <h5 class="font-weight-bold border-text text-white visible">NOS REFERENCES</h5>
+                <h1 class="font-weight-bold border-text text-white hidden">NOS REFERENCES</h1>
+            </div>
+
+            <div class="carousel-item active">
+                <img src="images/assets/excavator-modif.jpg" class="d-block w-100" alt="excavatrice">
+            </div>
+            <div class="carousel-item">
+                <img src="images/assets/slideshow2.jpg" class="d-block w-100" alt="camions">
+            </div>
+            <div class="carousel-item">
+                <img src="images/assets/slideshow3.jpg" class="d-block w-100" alt="pelleteuse">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!-- FIN CAROUSSEL -->
 
     <ul class="nav nav-tabs justify-content-around mt-1" id="myTab" role="tablist">
         <li class="nav-item">
